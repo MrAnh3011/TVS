c47d170861da72529be54c5a8149bf5e1391cca6
MrAnh3011 <tuananh30111@gmail.com>
2020-08-17 10:47:21 +0700

    update code 17/8

--- examples/MvcWeb/Startup.cs
+++ examples/MvcWeb/Startup.cs
@@ -35,6 +35,7 @@ namespace MvcWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddResponseCompression();
             services.AddLocalization(options =>
                 options.ResourcesPath = "Resources"
             );
@@ -71,6 +72,7 @@ namespace MvcWeb
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApi api)
         {
+            app.UseResponseCompression();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
